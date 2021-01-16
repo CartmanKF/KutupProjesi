0d51a953c5be20749c7bedac8e450d0a1e4bca9e
DESKTOP-LEQMJCA\Fidan <kutalmisalpfidan@gmail.com>
2021-01-16 23:38:58 +0300

    quiz eklendi

---
+++ Assets/kodlar/soruilkkisim.cs
@@ -0,0 +1,210 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class soruilkkisim : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("soru 1");
+    }
+    public void AnaMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void tekrardene()
+    {
+        SceneManager.LoadScene("soru 1");
+    }
+
+    public void Soru1A()
+    {
+        SceneManager.LoadScene("soru 2");
+    }
+    public void Soru1B()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+    public void Soru1C()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+    public void Soru1D()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+    public void Soru2A()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+    public void Soru2B()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+    public void Soru2C()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+    public void Soru2D()
+    {
+        SceneManager.LoadScene("soru 3");
+    }
+
+    public void Soru3A()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+    public void Soru3B()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+    public void Soru3C()
+    {
+        SceneManager.LoadScene("soru 4");
+    }
+    public void Soru3D()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+    public void Soru4A()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+    public void Soru4B()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+    public void Soru4C()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+    public void Soru4D()
+    {
+        SceneManager.LoadScene("soru 5");
+    }
+
+    public void Soru5A()
+    {
+        SceneManager.LoadScene("soru 6");
+    }
+    public void Soru5B()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+    public void Soru5C()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+    public void Soru5D()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+    public void Soru6A()
+    {
+        SceneManager.LoadScene("soru 7");
+    }
+    public void Soru6B()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+    public void Soru6C()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+    public void Soru6D()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+
+    public void Soru7A()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+    public void Soru7B()
+    {
+        SceneManager.LoadScene("soru 8");
+    }
+    public void Soru7C()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+    public void Soru7D()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+    public void Soru8A()
+    {
+        SceneManager.LoadScene("soru 9");
+    }
+    public void Soru8B()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+    public void Soru8C()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+    public void Soru8D()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+
+    public void Soru9A()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+    public void Soru9B()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+    public void Soru9C()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+    public void Soru9D()
+    {
+        SceneManager.LoadScene("soru 10");
+    }
+    public void Soru10A()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+    public void Soru10B()
+    {
+        SceneManager.LoadScene("tebrikler1");
+    }
+    public void Tebrikler2()
+    {
+        SceneManager.LoadScene("tebrikler2");
+    }
+    public void Soru10C()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+    public void Soru10D()
+    {
+        SceneManager.LoadScene("yanlisekrani");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

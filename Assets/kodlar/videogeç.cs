9b2db66369d9cf491971b026c7ed92ca3039ad33
DESKTOP-LEQMJCA\Fidan <kutalmisalpfidan@gmail.com>
2021-01-25 17:12:22 +0300

    BİTTİ 2

---
+++ Assets/kodlar/videogeç.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class videogeç : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(bekle());
+    }
+
+    IEnumerator bekle()
+    {
+        yield return new WaitForSeconds(23); 
+        SceneManager.LoadScene("oyun"); //scene ismi
+    }
+}
---
+++ Assets/kodlar/videogeç2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class videogeç2 : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(bekle());
+    }
+
+    IEnumerator bekle()
+    {
+        yield return new WaitForSeconds(70); 
+        SceneManager.LoadScene("kutupelcisi"); //scene ismi
+    }
+}
